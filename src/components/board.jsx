45b5f1ba643fbef385944619cf4a08cca7a5809a
lola <you@example.com>
2020-04-23 19:09:14 +0200

    advancing

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { getFigure } from '../utils/figures'
+// deep clone for prevent old state overriting the modified state
+import cloneDeep from 'lodash/cloneDeep'
+import { getFigure, getCenteredFigure } from '../utils/figures'
 import Row from './row'
 
 class Board extends Component {
@@ -9,30 +11,113 @@ class Board extends Component {
     this.state = {
       rows: 20,
       cols: 10,
-      figures: [
-        // getFigure('T', 'right'),
-        getFigure('L', 'left'),
-        [
-          [0, 9],
-          [1, 9],
-          [2, 9],
-        ],
+      currentFigure: [],
+      board: [
+        { x: 3, y: 3, color: 'red' },
+        { x: 4, y: 3, color: 'red' },
+        { x: 5, y: 3, color: 'red' },
+        { x: 3, y: 7, color: 'pink' },
+        { x: 3, y: 8, color: 'pink' },
       ],
     }
   }
 
-  print = (x, y) => {
-    let style = { background: 'transparent' }
+  componentDidMount() {
+    this.setRandomFigure()
+  }
 
-    this.state.figures.forEach((fig) => {
-      fig.forEach((coord) => {
-        if (coord[0] === x && coord[1] === y) {
-          console.log('match > ', x + y)
-          style.background = 'green'
+  generateFigure = (figure, position, color) => {
+    let items = []
+    // const shape = getCenteredFigure(figure, position)
+    const shape = getFigure(figure, position)
+    shape.map((coord) => {
+      items.push({
+        x: coord[0],
+        y: coord[1],
+        color,
+      })
+    })
+    return items
+  }
+
+  setRandomFigure = () => {
+    const currentFigure = this.generateFigure('T', 'left', 'green')
+    console.log(currentFigure)
+    this.setState({ currentFigure })
+  }
+
+  fillBg = (x, y) => {
+    // set filled default items
+    const { board, currentFigure } = this.state
+    let style = { background: 'transparent' }
+    board.forEach((figure) => {
+      if (figure.x === x && figure.y === y) {
+        style.background = figure.color
+      }
+    })
+    // set current figure
+    currentFigure.forEach((figure) => {
+      if (figure.x === x && figure.y === y) {
+        style.background = figure.color
+      }
+    })
+
+    return style
+  }
+
+  moveFigure = (_e) => {
+    // R-39, L-37, D-40
+    let increment = 0,
+      coord = 'x'
+
+    switch (_e.keyCode) {
+      case 39:
+        coord = 'x'
+        increment = 1
+        break
+      case 37:
+        coord = 'x'
+        increment = -1
+        break
+      case 40:
+        coord = 'y'
+        increment = 1
+        break
+      default:
+    }
+
+    let { board, currentFigure } = { ...this.state }
+
+    // let movedFigure = [...this.state.currentFigure] NOT WORKING
+    let movedFigure = cloneDeep(currentFigure)
+
+    movedFigure.forEach((fig) => (fig[coord] += increment))
+
+    let coincidences = false
+
+    board.forEach((boardUnit) => {
+      movedFigure.forEach((figUnit) => {
+        if (
+          (boardUnit.x === figUnit.x && boardUnit.y === figUnit.y) ||
+          figUnit.x < 0 ||
+          figUnit.x > this.state.cols - 1 ||
+          figUnit.y > this.state.rows - 1
+        ) {
+          console.log('match! ', [figUnit.x, figUnit.y])
+          coincidences = true
+        } else {
         }
       })
     })
-    return style
+
+    if (!coincidences) {
+      this.setState({ currentFigure: movedFigure })
+      coincidences = false
+    } else {
+      this.setState((prevState) => ({
+        currentFigure,
+      }))
+    }
   }
 
   render() {
@@ -41,25 +126,16 @@ class Board extends Component {
     const colArray = Array.from(new Array(cols).keys())
 
     return (
-      // <div className='board'>
-      //   {rowsArray.map((row, index) => (
-      //     <Row
-      //       columns={state.columns}
-      //       colored={index === state.element.x}
-      //       element={state.element}
-      //     />
-      //   ))}
-      // </div>
-      <div className='board'>
-        {rowsArray.map((row, x) => (
-          <div key={String(row + x)} className='row'>
-            {colArray.map((col, y) => (
+      <div className='board' onKeyDown={this.moveFigure} tabIndex='0'>
+        {rowsArray.map((row, y) => (
+          <div key={String(row + y)} className='row'>
+            {colArray.map((col, x) => (
               <div
-                key={String(col + y)}
+                key={String(col + x)}
                 className='hub'
                 x={x}
                 y={y}
-                style={this.print(x, y)}
+                style={this.fillBg(x, y)}
               >
                 {x},{y}
               </div>
--- src/utils/figures.js
+++ src/utils/figures.js
@@ -4,51 +4,51 @@ const figures = {
       [0, 1],
       [1, 0],
       [1, 1],
-      [1, 2],
-    ],
-    down: [
-      [0, 0],
-      [0, 1],
-      [0, 2],
-      [1, 1],
-    ],
-    left: [
-      [0, 1],
-      [1, 0],
-      [1, 1],
       [2, 1],
     ],
     right: [
+      [1, 0],
+      [1, 1],
+      [2, 1],
+      [1, 2],
+    ],
+    down: [
       [0, 1],
       [1, 1],
       [1, 2],
       [2, 1],
+    ],
+    left: [
+      [1, 0],
+      [0, 1],
+      [1, 1],
+      [1, 2],
     ],
   },
   L: {
     up: [
-      [0, 0],
-      [1, 0],
-      [2, 0],
-      [2, 1],
-    ],
-    right: [
-      [0, 0],
-      [0, 1],
-      [0, 2],
-      [1, 0],
-    ],
-    down: [
-      [0, 0],
-      [0, 1],
-      [1, 1],
-      [2, 1],
-    ],
-    left: [
-      [0, 2],
       [1, 0],
       [1, 1],
       [1, 2],
+      [2, 2],
+    ],
+    right: [
+      [0, 1],
+      [1, 1],
+      [0, 2],
+      [2, 1],
+    ],
+    down: [
+      [0, 0],
+      [1, 0],
+      [1, 1],
+      [1, 2],
+    ],
+    left: [
+      [0, 0],
+      [0, 1],
+      [1, 1],
+      [2, 1],
     ],
   },
 }
@@ -57,4 +57,11 @@ function getFigure(figure, position = null) {
   return figures[figure][position ? position : 'up']
 }
 
-export { figures, getFigure }
+function getCenteredFigure(figure) {
+  let selected = figures[figure]['up']
+  selected.map((fig) => (fig[0] += 3))
+  console.log(selected)
+  return selected
+}
+
+export { figures, getFigure, getCenteredFigure }
