ab60f7f809b7881b72cb045d4dd01754d07439e5
lola <you@example.com>
2020-05-18 09:49:38 +0200

    buttons and colors

--- src/components/controls.jsx
+++ src/components/controls.jsx
@@ -23,7 +23,17 @@ class Controls extends Component {
             style={this.addStyle(pressedButton.left)}
             onMouseDown={(_e) => keyPress(_e, 'left')}
           >
-            <span className='arrow'> &#8598;</span>
+            <span className='arrow'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='white'
+                width='1.5rem'
+                height='1.5rem'
+              >
+                <path d='M15.41 16.59L10.83 12l4.58-4.59L14 6l-6 6 6 6 1.41-1.41z' />
+              </svg>
+            </span>
           </button>
           <button
             type='button'
@@ -31,7 +41,17 @@ class Controls extends Component {
             style={this.addStyle(pressedButton.down)}
             onMouseDown={(_e) => keyPress(_e, 'down')}
           >
-            <span className='arrow'> &#8598;</span>
+            <span className='arrow'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='white'
+                width='1.5rem'
+                height='1.5rem'
+              >
+                <path d='M7.41 8.59L12 13.17l4.59-4.58L18 10l-6 6-6-6 1.41-1.41z' />
+              </svg>
+            </span>
           </button>
           <button
             type='button'
@@ -39,7 +59,17 @@ class Controls extends Component {
             style={this.addStyle(pressedButton.right)}
             onMouseDown={(_e) => keyPress(_e, 'right')}
           >
-            <span className='arrow'> &#8598;</span>
+            <span className='arrow'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='white'
+                width='1.5rem'
+                height='1.5rem'
+              >
+                <path d='M8.59 16.59L13.17 12 8.59 7.41 10 6l6 6-6 6-1.41-1.41z' />
+              </svg>
+            </span>
           </button>
         </div>
 
@@ -59,7 +89,18 @@ class Controls extends Component {
             style={this.addStyle(pressedButton.twist)}
             onMouseDown={(_e) => keyPress(_e, 'rotate')}
           >
-            <span className='arrow'> &#8634;</span>
+            {/* <span className='arrow'> &#8634;</span> */}
+            <span className='arrow'>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                fill='white'
+                width='1.5rem'
+                height='1.5rem'
+              >
+                <path d='M12,5V1L7,6l5,5V7c3.31,0,6,2.69,6,6s-2.69,6-6,6s-6-2.69-6-6H4c0,4.42,3.58,8,8,8s8-3.58,8-8S16.42,5,12,5z' />
+              </svg>
+            </span>
           </button>
 
           {(displayHold && (
@@ -68,7 +109,7 @@ class Controls extends Component {
               style={this.addStyle(pressedButton.hold)}
               onMouseDown={(_e) => keyPress(_e, 'hold')}
             >
-              hold
+              <span className='text'>hold</span>
             </button>
           )) || (
             <button
@@ -76,7 +117,7 @@ class Controls extends Component {
               style={this.addStyle(pressedButton.hold)}
               onMouseDown={(_e) => keyPress(_e, 'use')}
             >
-              use
+              <span className='text'>use</span>
             </button>
           )}
         </div>
